a580dc80f85887e49186613b5218822f35ae9673
Ramsey <416034116@qq.com>
2020-10-26 00:47:23 -0300

    update main.app

--- SecondAlgorithm/main.cpp
+++ SecondAlgorithm/main.cpp
@@ -58,5 +58,105 @@ int main() {
     delete[] array;
     return 0;
 }
+int Timothy_Second(int array[], int len, int s)
+{
+    int start_index = 0;
+    int end_index = 0;
+    int *tiny_query = new int[2000];
+    int *med_query = new int[2000];
+    int *huge_query = new int[2000];
+    int *result = new int[3000];
+    // int second_point = rand_point(len);
+    generate_query(tiny_query, med_query, huge_query, 2000);
+    int  t = (int)ceil((float)len/(float)s);
+    double vm, rss;
+    process_mem_usage(vm, rss);
+    cout << "Start:" << "  " << "VM: " << vm << "; RSS: " << rss << endl;
+    set<int> D;
+    for (int i = 0; i < len; i++) {
+        D.insert(array[i]);
+    }
+    /*set<int>::iterator iter = D.begin();
+    while (iter != D.end()) {
+       // cout << *iter++ << ' ';
+    }
+    cout << endl;*/
+    process_mem_usage(vm, rss);
+    cout << "D peak usage =:" << "  " << "VM: " << vm << "; RSS: " << rss << endl;
+    int delta = D.size();  // save the value of delta
+    int *upper_line_array = new int[len];  // upper_line_array
+    map<int, int> setMap;
+    int count1 = 1;
+    set<int>::iterator it;
+    for(it=D.begin();it!=D.end();it++)  //get the upper_line_array  nlogΔ
+    {
+        setMap.insert(make_pair(*it, count1));
+        count1++;
+    }
+    for (int i = 0; i < len; i++) {
+        upper_line_array[i] = setMap[array[i]];
+    }  // the end of getting upper_line_array
+    /* for (int i = 0; i < len; i++) {      // get the upper_line_array
+         int count = 1;
+         set<int>::iterator iter = D.begin();
+         while (iter != D.end()) {
+             if (*iter == array[i]) {
+                 upper_line_array[i] = count;
+                 break;
+             }
+             *iter++;
+             count++;
+         }
+     }   // the end of getting the upper_line_array
+     */
+    /*  for(int i : upper_line_array) {
+          cout << i << "  ";
+      }
+      cout << endl;*/
+    map<int,int> QaMap;
+    int *A_Prime = new int[len];
+    for (int i = 0; i < len; i++) {  //traverse the upper_line_array for getting the length of every row in Qa
+        QaMap[upper_line_array[i]]++;
+        A_Prime[i] = QaMap[upper_line_array[i]] - 1;
+    } //the end of getting the length of every row in Qa
+    /* for (int x : A_Prime) {
+         cout << x <<"  ";
+     }*/
 
+    process_mem_usage(vm, rss);
+    cout << "QaMap peak usage =:" << "  " << "VM: " << vm << "; RSS: " << rss << endl;
+
+    //int Qa[delta][currentMax];
+    //cout <<"delta: " << delta << " currentMax: " << currentMax;
+    int **Qa = new int*[delta];            // initialize 2-D dynamic array Qa
+    for (int i = 0; i < delta; i++) {
+        Qa[i] = new int[QaMap[i + 1]];
+    }
+    std::map<int,std::list<int>> index_upper_line_array;    // create the map for creating the Qa
+    for (int i = 0; i < len; i++) {
+        index_upper_line_array[upper_line_array[i]].push_back(i + 1);
+    }
+       /*for (auto itr : index_upper_line_array){
+           cout << itr.first << ":\t" ;
+           for (auto innerItr : itr.second)
+               cout << innerItr << " ";
+           cout << endl;
+       }*/
+    for (auto itr : index_upper_line_array){      // update the value into the 2-D array
+        int j = 0;
+        for (auto innerItr : itr.second)
+            Qa[itr.first-1][j++] = innerItr;
+    }
+    process_mem_usage(vm, rss);
+    cout << "Qa peak usage =:" << "  " << "VM: " << vm << "; RSS: " << rss << endl;
+
+    int *bitSize = new int[s];
+    int totalBitSize = getSize(upper_line_array, bitSize, s, t, len);
+    //int_vector<> word(ceil(totalBitSize / 32.0) + 1, 0, 32);
+    std::vector<unsigned int> word(ceil(totalBitSize / 32.0) + 1);
+    //vector<unsigned int> word;
+    int currentSize = 0, currentIndex = 1, newMethod = 0;
+    int blocks = ceil(len / t + 0.0);
+    int diff = s - blocks;
+}
    
