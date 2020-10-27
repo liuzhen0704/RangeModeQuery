cfd5a57908865ba444820ab9a535737a7055748c
Ramsey <416034116@qq.com>
2020-10-27 02:46:55 -0300

    add two function

--- SecondAlgorithm/main.cpp
+++ SecondAlgorithm/main.cpp
@@ -159,4 +159,55 @@ int Timothy_Second(int array[], int len, int s)
     int blocks = ceil(len / t + 0.0);
     int diff = s - blocks;
 }
+void read_file(int array[], string file, int len) {
+    ifstream file_in(file);
+    int temp;
+    if (!file_in.is_open()) {
+        cout << " cannot open the file" << endl;
+    }
+    for (int i = 0; i < len; i++) {
+        file_in >> array[i];
+    }
+    /*     for (int i = 0; i < len; i++) {
+             cout << array[i] << " ";
+         }*/
+    file_in.close();
+    file_in.clear();
+}
+void process_mem_usage(double& vm_usage, double& resident_set)
+{
+    using std::ios_base;
+    using std::ifstream;
+    using std::string;
+
+    vm_usage     = 0.0;
+    resident_set = 0.0;
+
+    // 'file' stat seems to give the most reliable results
+    //
+    ifstream stat_stream("/proc/self/stat",ios_base::in);
+
+    // dummy vars for leading entries in stat that we don't care about
+    //
+    string pid, comm, state, ppid, pgrp, session, tty_nr;
+    string tpgid, flags, minflt, cminflt, majflt, cmajflt;
+    string utime, stime, cutime, cstime, priority, nice;
+    string O, itrealvalue, starttime;
+
+    // the two fields we want
+    //
+    unsigned long vsize;
+    long rss;
+
+    stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr
+                >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt
+                >> utime >> stime >> cutime >> cstime >> priority >> nice
+                >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
+
+    stat_stream.close();
+
+    long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
+    vm_usage     = vsize / 1024.0;
+    resident_set = rss * page_size_kb;
+}
    
